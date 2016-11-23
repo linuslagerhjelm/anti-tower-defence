1273485fccfe1d3985718b844ce3c45aaa97e895
ganiester <andreas.d.wendel@gmail.com>
2016-11-23 13:59:20 +0100

    Ett till test!

--- AntiTowerIntelij/src/Main.java
+++ AntiTowerIntelij/src/Main.java
@@ -1,17 +1,12 @@
 /**
  * Created by AnonM on 2016-11-23.
-
-
-
  */
-
-
-
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
 	System.out.print("Fiskare");
             // en ändring
+	//en till ändring!!¤!"¤!"¤!2
     }
 }
