3baca69c52f40e608ffe870a798985a47adbf904
c15aen <arvid@ernstsson@icloud.com>
2016-11-24 14:28:02 +0100

    New image

--- src/view/Main.java
+++ src/view/Main.java
@@ -3,13 +3,13 @@ package view;
 import view.MainWindow;
 
 /**
- * Created by AnonM on 2016-11-23.
+ * Created by AnonM on 2016-11-23.54ra ereat
  */
 public class Main {
 
     public static void main(String[] args) {
 
-            MainWindow main = new MainWindow("Title", 500, 500);
+            MainWindow main = new MainWindow("Title", 700, 700);
             main.show();
     }
 }
