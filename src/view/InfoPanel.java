bb45f28279ea5e82ce73c41f3da42081f959a091
Andreas Wendel <c15awl@cs.umu.se>
2016-12-14 14:10:56 +0100

    Info panel comments

--- src/view/InfoPanel.java
+++ src/view/InfoPanel.java
@@ -3,8 +3,13 @@ package view;
 import javax.swing.*;
 import java.awt.*;
 
+
 /**
- * Created by c15aen on 2016-11-23. INTE DEFAULT, SLUTA KLAGA!
+ * Author: Andreas, Arvid
+ * File: InfoPanel
+ * Created: 16-11-23
+ * Description: Information panel to display money,
+ * highscore and etc.
  */
 public class InfoPanel {
 
@@ -48,41 +53,68 @@ public class InfoPanel {
                 passedField.setEditable(false);
 
         }
-        public JPanel getPanel() {
 
+        /**
+         * GetPanel returns the info menu's jPanel to be placed in as a component.
+         * @return infoPanel:JPanel
+         */
+        public JPanel getPanel() {
                 return infoPanel;
         }
 
+        /**
+         * Sets the current score to the given integer value
+         * @param newCurrentScore:int, score to be set.
+         */
         public void setScore(int newCurrentScore) {
-
                 currentScore = newCurrentScore;
         }
 
+        /**
+         * Add score adds to the current score by the given value
+         * of the argument integer.
+         * @param scoreToAdd:int, value of score to add.
+         */
         public void addScore(int scoreToAdd) {
-
                 currentScore += scoreToAdd;
         }
 
+        /**
+         * Sets the value of the highscore field, with the given
+         * argument integer value.
+         * @param newHighScore:int, new highscore to be displayed.
+         */
         public void setHighScore(int newHighScore) {
-
                 highScore = newHighScore;
         }
 
+        /**
+         * Sets the display text of required passes to be made to win the level.
+         * @param newRequiredPasses:int, number value of number of passes to be made.
+         */
         public void setRequiredPasses(int newRequiredPasses) {
-
                 requiredPasses = newRequiredPasses;
         }
 
+        /**
+         * Increase the display of number of passes that has been made
+         */
         public void incrementPassed() {
-
                 passed++;
         }
 
+        /**
+         * Sets the display number of number of passes that has been made.
+         * @param newPassed:int, nr of passes made to be displayed.
+         */
         public void setPassed(int newPassed) {
-
                 passed = newPassed;
         }
 
+        /**
+         * sets the display number of the amount of money the user has.
+         * @param newMoney:int, amount of money to be displayed.
+         */
         public void setMoney(String newMoney) {
             SwingUtilities.invokeLater(() -> {
                 money = newMoney;
@@ -90,6 +122,9 @@ public class InfoPanel {
             });
         }
 
+        /**
+         * Display info displays the information on the information panel.
+         */
         private void displayInfo() {
                 highScoreField.setText(""+highScore);
                 currentScoreField.setText(""+currentScore);
